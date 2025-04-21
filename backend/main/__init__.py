be9aac44cc75990c123d0a8a3215fd733384f541
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2025-04-21 19:29:33 -0300

    Empezando a hacer el TP3

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from dotenv import load_dotenv
 
 from flask_restful import Api
+import os
+from flas_sqlalchemy import SQLAlchemy
 
 from resources import (
     OrderResource,
@@ -15,10 +17,21 @@ from resources import (
 
 api = Api()
 
+#Inicializamos la db con sqlalchemy
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     load_dotenv()
+
+
+    if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_USI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    db.init_app(app)
+    
 
     api.add_resource(UserResource, "/users/<int:id>")
     api.add_resource(ProductResource, "/products/<int:id>")
