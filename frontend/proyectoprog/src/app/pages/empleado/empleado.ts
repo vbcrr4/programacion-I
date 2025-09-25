95a930bf787f121ede8f6c270e55429ebf78b016
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2025-09-24 21:33:51 -0300

    setup de angular

---
+++ frontend/proyectoprog/src/app/pages/empleado/empleado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  imports: [],
+  templateUrl: './empleado.html',
+  styleUrl: './empleado.css'
+})
+export class Empleado {
+
+}
