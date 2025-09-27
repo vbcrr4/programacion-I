8619bbe72b6f97b9029ae374b89a67f287a01c46
Nehuen <n.donozo@alumno.um.edu.ar>
2025-09-26 23:53:16 -0300

    panel empleado listo, esperemos

--- frontend/proyectoprog/src/app/pages/empleado/empleado-panel/empleado-panel.ts
+++ frontend/proyectoprog/src/app/pages/empleado/empleado-panel/empleado-panel.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink} from '@angular/router';
+
 @Component({
   selector: 'app-empleado-panel',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [ RouterLink],
   templateUrl: './empleado-panel.html',
-  styleUrl: './empleado-panel.css'
+  styleUrls: ['./empleado-panel.css'] 
 })
-export class EmpleadoPanel {
-
-}
+export class EmpleadoPanel {}
