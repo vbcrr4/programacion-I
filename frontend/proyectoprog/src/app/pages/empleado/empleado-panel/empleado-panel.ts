680732892ccccc52228f03b868f4db2507a34583
Nehuen <n.donozo@alumno.um.edu.ar>
2025-09-26 22:36:56 -0300

    panel empleado

--- frontend/proyectoprog/src/app/pages/empleado/empleado-panel/empleado-panel.ts
+++ frontend/proyectoprog/src/app/pages/empleado/empleado-panel/empleado-panel.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from "../../admin/admin-menu/admin-routing.routes";
 
 @Component({
   selector: 'app-empleado-panel',
-  imports: [],
+  imports: [AppRoutingModule],
   templateUrl: './empleado-panel.html',
   styleUrl: './empleado-panel.css'
 })
