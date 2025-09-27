3d4fe64f4903a2f54a5510143d25b40c6566444a
Nehuen <n.donozo@alumno.um.edu.ar>
2025-09-27 01:02:56 -0300

    estado pedido, ready

--- frontend/proyectoprog/src/app/app.routes.ts
+++ frontend/proyectoprog/src/app/app.routes.ts
@@ -1,12 +1,14 @@
+import { Pedidos } from './pages/admin/pedidos/pedidos';
 import { Routes } from '@angular/router';
+
 export const routes: Routes = [
 {path:'login', loadComponent: () => import('./pages/auth/login/login').then(m => m.Login)},
 {path:'register', loadComponent: () => import('./pages/auth/register/register').then(m => m.Register)},
 {path:'admin-menu', loadComponent: () => import('./pages/admin/admin-menu/admin-menu').then(m => m.AdminMenu)},
 {path:'menu', loadComponent: () => import('./pages/cliente/menu/menu').then(m => m.Menu)},
 {path:'empleado-panel', loadComponent: () => import('./pages/empleado/empleado-panel/empleado-panel').then(m => m.EmpleadoPanel)},
+{path:'estado-pedido', loadComponent: () => import('./pages/empleado/estado-pedido/estado-pedido').then(m => m.EstadoPedido)},
 {path:'error', loadComponent: ()=>import('./pages/error-page/error-page').then(m=>m.ErrorPage)},
 {path:'', redirectTo: 'login', pathMatch: 'full'},
-{path:'**',redirectTo: 'error'}
-
+{path:'**',redirectTo: 'error'},
 ];
--- frontend/proyectoprog/src/app/pages/empleado/estado-pedido/estado-pedido.ts
+++ frontend/proyectoprog/src/app/pages/empleado/estado-pedido/estado-pedido.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-estado-pedido',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './estado-pedido.html',
-  styleUrl: './estado-pedido.css'
+  styleUrls: ['./estado-pedido.css']
 })
-export class EstadoPedido {
-
-}
+export class EstadoPedido {}
