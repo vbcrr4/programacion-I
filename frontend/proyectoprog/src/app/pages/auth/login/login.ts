e8d367c2b2e3024e11b364cf69638772bd5b4f27
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2025-10-23 22:02:43 -0300

    refactorizando botones, no tocar nada porfa

--- frontend/proyectoprog/src/app/pages/auth/login/login.ts
+++ frontend/proyectoprog/src/app/pages/auth/login/login.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { UniversalCard } from '../../../components/universal-card/universal-card';
 import { InputField } from '../../../components/input/input';
 import { ButtonField } from '../../../components/button/button';
+import { ActionBttn } from '../../../components/action-bttn/action-bttn';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, UniversalCard, InputField, ButtonField],
+  imports: [RouterLink, UniversalCard, InputField, ButtonField,ActionBttn],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
