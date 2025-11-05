11685dc0f75e8ff96666b4dcd7c20f8d4ad84d16
Nehuen <n.donozo@alumno.um.edu.ar>
2025-11-05 19:52:28 -0300

    calificar

--- frontend/proyectoprog/src/app/pages/carrito/carrito.ts
+++ frontend/proyectoprog/src/app/pages/carrito/carrito.ts
@@ -4,22 +4,22 @@ import { UniversalCard } from "../../components/universal-card/universal-card";
 import { Card } from "../../components/card/card";
 import { ActionBttn } from '../../components/action-bttn/action-bttn';
 import { TextArea } from "../../components/textarea/textarea";
+import { CalifiEstrellas } from '../../components/calfiestrellas/calfiestrellas';
 import { OrderService } from '../../services/order.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [Navbar, UniversalCard, ActionBttn, TextArea, Card, CommonModule],
+  imports: [Navbar, UniversalCard, ActionBttn, TextArea, Card, CommonModule, CalifiEstrellas],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css'
 })
 export class Carrito implements OnInit {
   orders: any[] = [];
-  status = "Listo"
-  del_status = "En camino"
-  del_price = "$5.000"
-  del_address = "Av. Boulogne Sur Mer 683, M5500 Mendoza"
+  del_status: string = '';
+  del_price: string = '$5.000'; // Assuming a fixed price for now
+  del_address: string = '';
   tabseleccionada = "pedido"
 
   constructor(private orderService: OrderService) { }
@@ -32,6 +32,10 @@ export class Carrito implements OnInit {
     this.orderService.getOrders().subscribe({
       next: (response) => {
         this.orders = response.orders;
+        if (this.orders.length > 0) {
+          this.del_status = this.orders[0].status;
+          this.del_address = this.orders[0].user?.address;
+        }
       },
       error: (err) => {
         console.error('Error loading orders', err);
