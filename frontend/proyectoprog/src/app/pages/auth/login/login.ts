790582cb84aef1c47860f5f0563d7a19e9a97d41
Nai <62731284+tobiastka@users.noreply.github.com>
2025-11-06 07:59:21 -0300

    Alert on login

--- frontend/proyectoprog/src/app/pages/auth/login/login.ts
+++ frontend/proyectoprog/src/app/pages/auth/login/login.ts
@@ -5,16 +5,18 @@ import { InputField } from '../../../components/input/input';
 import { ActionBttn } from '../../../components/action-bttn/action-bttn';
 import { AuthService } from '../../../services/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, UniversalCard, InputField, ActionBttn, ReactiveFormsModule],
+  imports: [RouterLink, UniversalCard, InputField, ActionBttn, ReactiveFormsModule, NgIf],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class Login {
   loginForm: FormGroup;
+  errorMessage: string = '';
 
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
@@ -25,11 +27,17 @@ export class Login {
 
   onSubmit(): void {
     if (this.loginForm.valid) {
+      this.errorMessage = '';
       this.authService.login(this.loginForm.value).subscribe({
         next: () => {
           this.router.navigate(['/menu']);
         },
         error: (err) => {
+          if (err.status === 401) {
+            this.errorMessage = 'Usuario o contraseña incorrecta';
+          } else {
+            this.errorMessage = 'Ocurrió un error. Por favor, intente nuevamente';
+          }
           console.error('Login failed', err);
         }
       });
